7c740b77ad9b7d0cf26007f9b1c886aff797b03a
zeeshanmunshi0 <110233177+zeeshanmunshi0@users.noreply.github.com>
2022-07-29 17:25:32 +0530

    Update streamlit_app.py
    added emoji & create new

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ streamlit.header('Breakefast Menu')
 streamlit.text('Omega 3 Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
+
+streamlit.title('My Mom's New Healthy Diner')
+
+streamlit.header('Breakefast Favorites')
+streamlit.text('🥣 Omega 3 Blueberry Oatmeal')
+streamlit.text('🥗 Kale Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
