94c75e4db22732f2f878ea07a08c712264373d83
zeeshanmunshi0 <110233177+zeeshanmunshi0@users.noreply.github.com>
2022-08-02 15:09:06 +0530

    Update streamlit_app.py
    Temp change

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
